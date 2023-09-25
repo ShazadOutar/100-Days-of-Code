2febd91e7ba02cba2e2e03b1d16daa71b533fefd
ShazadO <shazadoutar8@gmail.com>
2023-09-25 02:42:36 -0400

    Setting up for Day 8

---
+++ Day8/exercise1.py
@@ -0,0 +1,10 @@
+# Review: 
+# Create a function called greet(). 
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+
+def greet():
+    for i in range(3):
+        print("Hello")
+
+greet()
