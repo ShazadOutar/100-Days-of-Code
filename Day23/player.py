9924537f2305e9ab19591a2311505c011ada5c19
ShazadO <shazadoutar8@gmail.com>
2023-10-16 05:11:57 -0400

    Day 23 Progress

---
+++ Day23/player.py
@@ -0,0 +1,20 @@
+"""Player Turtle File"""
+from turtle import Turtle
+STARTING_POSITION = (0, -280)
+MOVE_DISTANCE = 10
+FINISH_LINE_Y = 280
+
+
+class Player(Turtle):
+    # create the player at the bottom and give it a way to move forward
+    def __init__(self):
+        super().__init__("turtle")
+        self.penup()
+        self.goto(STARTING_POSITION)
+        self.left(90)
+
+    def move(self):
+        self.forward(MOVE_DISTANCE)
+
+    def new_level(self):
+        self.goto(STARTING_POSITION)
