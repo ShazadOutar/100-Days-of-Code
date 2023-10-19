7af973c0c91ee8a863aac26a7bb61839f2ce987c
ShazadO <shazadoutar8@gmail.com>
2023-10-18 23:21:49 -0400

    Day 24 completed
    Reading and writing to files using Python.

--- Day24/Mail Merge Project Start/main.py
+++ Day24/Mail Merge Project Start/main.py
@@ -9,6 +9,24 @@
 
 # names = []
 with open("./Input/Names/invited_names.txt") as file:
-    names = file.readlines()
+    names_input = file.readlines()
 
-print(names)
+print(type(names_input[0]))
+# remove the \n from each name
+names = []
+for name in names_input:
+    names.append(name.replace("\n", ""))
+
+print(f"New names list {names}")
+
+# get the letter template
+with open("./Input/Letters/starting_letter.txt") as file:
+    letter = file.read()
+
+print(letter)
+for name in names:
+    with open(f"./Output/ReadyToSend/{name}.txt", mode="w+") as file:
+        # file.write(f"Hello {name}")
+        temp = letter.replace(f"[name]", f"{name}")
+        print(f"new temp is {temp}")
+        file.write(temp)
