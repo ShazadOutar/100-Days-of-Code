1f7114a5ead103c41d17c569599ebfa036be0e66
ShazadO <shazadoutar8@gmail.com>
2023-11-25 23:53:00 -0500

    Day 39 Progress

--- Day39-Flight-Deal-Finder/main.py
+++ Day39-Flight-Deal-Finder/main.py
@@ -1,16 +1,41 @@
 from data_manager import DataManager
 from pprint import pprint
 from flight_search import FlightSearch
+from notification_manager import NotificationManager
+from flight_data import FlightData
 
 # This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to
 # achieve the program requirements.
 
-# Read the sheet and print what the sheet currently has
-sheet = DataManager()
+# # Read the sheet and print what the sheet currently has
+# sheet = DataManager()
+# # sheet.update_iata_codes()
+# sheet_data = sheet.read_sheet()
 # sheet.update_iata_codes()
-sheet_data = sheet.read_sheet()
-sheet.update_iata_codes()
-pprint(sheet_data)
+# pprint(sheet_data)
 
 # search = FlightSearch()
 # search.get_trip_price("PAR")
+
+def main():
+    # Read the sheet and print what the sheet currently has
+    # sheet = DataManager()
+    # sheet_data = sheet.read_sheet()
+    # sheet.update_iata_codes()
+    # pprint(sheet_data)
+    # email = NotificationManager()
+    # email.send_email()
+    # search for discounts first, update new ones
+
+    # read the sheet data
+    # sheet_data = DataManager().read_sheet()
+    FlightData().find_discounted()
+
+    # email discount info
+    # email = NotificationManager().send_email()
+
+
+
+
+if __name__ == "__main__":
+    main()
--- Day39-Flight-Deal-Finder/notification_manager.py
+++ Day39-Flight-Deal-Finder/notification_manager.py
@@ -1,3 +1,16 @@
+import smtplib
+
+
+
 class NotificationManager:
-    #This class is responsible for sending notifications with the deal flight details.
-    pass
+    # This class is responsible for sending notifications with the deal flight details.
+    def send_email(self):
+        body = "body"
+        with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+            connection.starttls()
+            connection.login(user=sending_email, password=email_password)
+            connection.sendmail(
+                from_addr=sending_email,
+                to_addrs=receiving_email,
+                msg=f"Subject:Trip Discount\n\n{body}"
+            )
