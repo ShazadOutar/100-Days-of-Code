f491147b81579fef609a9945fec16819728f15d4
ShazadO <shazadoutar8@gmail.com>
2023-12-05 23:51:08 -0500

    Day 46 Progress

--- Day46-Time-Spotify-Playlist/main.py
+++ Day46-Time-Spotify-Playlist/main.py
@@ -1,25 +1,75 @@
 from bs4 import BeautifulSoup
 import requests
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+import os
 
-# input_date = input("Which date to use? Please enter a date in the format YYYY-MM-DD")
-input_date = "2022-11-19"
+CLIENT_ID = os.environ["SPOTIPY_CLIENT_ID"]
+CLIENT_SECRET = os.environ["SPOTIPY_CLIENT_SECRET"]
+REDIRECT_URL = os.environ["SPOTIPY_REDIRECT_URI"]
+SPOTIFY_USERNAME = os.environ["Spotify_Username"]
 
-response = requests.get(url=f"https://www.billboard.com/charts/hot-100/{input_date}")
-billboard_site = response.text
 
-soup = BeautifulSoup(billboard_site, "html.parser")
-# print(soup.prettify())
-# songs = soup.find_all(name="h3", id="title-of-a-story")
-# print(songs)
-# print(len(songs))
-# print(songs[0].getText())
-# print(soup.find(name="h3", id="title-of-a-story"))
-# print(songs[3])
+def get_date():
+    # TODO: make this an input later
+    # input_date = input("Which date to use? Please enter a date in the format YYYY-MM-DD")
+    input_date = "2022-11-19"
+    return input_date
 
-# print(soup.select_one(selector="body"))
-print(soup.find(name="div", class_="o-chart-results-list-row-container").find_all(name="li")[3].find(name="h3").getText().strip())
-song_tags = soup.find_all(name="div", class_="o-chart-results-list-row-container")
-print(len(song_tags))
-song_names = [song.find_all(name="li")[3].find(name="h3").getText().strip() for song in song_tags]
-print(song_names)
 
+def get_top_100_at_date(input_date: str) -> list:
+    # Use requests to get the page content
+    response = requests.get(url=f"https://www.billboard.com/charts/hot-100/{input_date}")
+    billboard_site = response.text
+
+    # parse the html data returned
+    soup = BeautifulSoup(billboard_site, "html.parser")
+
+    song_tags = soup.find_all(name="div", class_="o-chart-results-list-row-container")
+    # print(len(song_tags))
+    song_names = [song.find_all(name="li")[3].find(name="h3").getText().strip() for song in song_tags]
+    print(f"{len(song_names)} in songs_list")
+    return song_names
+
+
+def get_user_id(my_spotipy):
+    user_id = my_spotipy.current_user()["id"]
+    return user_id
+
+
+def create_playlist(my_spotipy, user_id, name="100 Days of Code", description="Day 46 First Try"):
+    my_spotipy.user_playlist_create(user=user_id,
+                                    name=name,
+                                    public=False,
+                                    description=description)
+
+
+def main():
+    # handle the date and getting the songs from the top 100
+    input_date = get_date()
+    songs_list = get_top_100_at_date(input_date=input_date)
+    print(songs_list)
+
+    # handle making the playlist and adding the songs
+    # Connect with spotipy to authorize with Spotify
+    scope = "playlist-modify-private playlist-read-private"
+    sp = spotipy.Spotify(
+        auth_manager=SpotifyOAuth(
+            scope=scope,
+            redirect_uri=REDIRECT_URL,
+            client_id=CLIENT_ID,
+            client_secret=CLIENT_SECRET,
+            show_dialog=True,
+            cache_path="token.txt",
+            username="ShazadOutar"
+        )
+    )
+
+    user_id = get_user_id(sp)
+    create_playlist(sp, user_id=user_id)
+
+    # after creating the playlist, get its id to reference later
+    # TODO: Add to the playlist next
+
+if __name__ == "__main__":
+    main()
--- Day46-Time-Spotify-Playlist/testing_spotipy.py
+++ Day46-Time-Spotify-Playlist/testing_spotipy.py
@@ -1,5 +1,5 @@
 import spotipy
-from spotipy.oauth2 import SpotifyOAuth, SpotifyClientCredentials
+from spotipy.oauth2 import SpotifyOAuth
 import os
 
 # auth_manager = SpotifyClientCredentials(client_id=os.environ["ClientID"], client_secret=os.environ["ClientSecret"])
@@ -10,16 +10,66 @@ import os
 CLIENT_ID = os.environ["SPOTIPY_CLIENT_ID"]
 CLIENT_SECRET = os.environ["SPOTIPY_CLIENT_SECRET"]
 REDIRECT_URL = os.environ["SPOTIPY_REDIRECT_URI"]
-
+SPOTIFY_USERNAME = os.environ["Spotify_Username"]
 # print(f"{CLIENT_ID}\n{CLIENT_SECRET}\n{REDIRECT_URL}")
 # scope = "user-library-read"
 # sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 # results = sp.current_user_saved_tracks()
 # print(results)
 
-# TODO: Fix this so it works properly, it's giving a service not found error
-scope = "playlist-modify-private"
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
-results = sp.user_playlist_create(user=sp.current_user(), name="100 Days of Code",
-                                  public=False, collaborative=False, description="")
-print(results)
+scope = "playlist-modify-private playlist-read-private"
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        scope=scope,
+        redirect_uri=REDIRECT_URL,
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET,
+        show_dialog=True,
+        cache_path="token.txt",
+        username="ShazadOutar"
+    )
+)
+
+user_id = sp.current_user()["id"]
+print(user_id)
+
+
+# print(sp.me()["id"])
+
+
+def create_playlist():
+    sp.user_playlist_create(user=user_id,
+                            name="100 Days of Code",
+                            public=False,
+                            description="Day 46 First Try")
+
+
+# create_playlist()
+playlists = sp.current_user_playlists()
+print(playlists)
+date_playlist = playlists["items"][0]
+print(date_playlist)
+date_playlist_id = date_playlist["id"]
+print(date_playlist_id)
+
+
+def get_track_id(song_name) -> str:
+    track = sp.search(q=song_name, type="track")["tracks"]["items"][0]["id"]
+    print(track)
+    return track
+
+
+def get_track_ids(songs_list) -> list:
+    song_ids = []
+    for song in songs_list:
+        track_id = sp.search(q=song, type="track")["tracks"]["items"][0]["id"]
+        print(track_id)
+        song_ids.append(track_id)
+    return song_ids
+tracks = ['Anti-Hero', 'Rich Flex', 'Major Distribution', 'On BS', 'Spin Bout U', 'Pussy & Millions', 'Privileged Rappers', 'Circo Loco', 'BackOutsideBoyz', 'Unholy', 'Hours In Silence', 'Broke Boys', 'Bad Habit', 'Treacherous Twins', 'Middle Of The Ocean', 'Jumbotron Shit Poppin', 'As It Was', "More M's", "I Guess It's Fuck Me", "I'm Good (Blue)", 'Super Freaky Girl', 'Lift Me Up', 'Lavender Haze', 'I Like You (A Happier Song)', 'You Proof', 'Something In The Orange', '3AM On Glenwood', "I Ain't Worried", 'Vegas', 'Midnight Rain', 'Jimmy Cooks', 'Under The Influence', 'Bejeweled', 'Die For You', 'Sunroof', 'Maroon', 'Wasted On You', 'Cuff It', 'Karma', 'Snow On The Beach', 'Wait For U', "You're On Your Own, Kid", 'Titi Me Pregunto', 'Just Wanna Rock', 'The Kind Of Love We Make', 'Shirt', 'She Had Me At Heads Carolina', 'About Damn Time', 'Tomorrow 2', 'Unstoppable', 'Vigilante Shit', 'Thank God', 'Die For You', '5 Foot 9', 'Hold Me Closer', 'Question...?', 'Golden Hour', 'Rock And A Hard Place', 'Fall In Love', 'Half Of Me', 'Made You Look', 'Hotel Lobby (Unc And Phew)', 'What My World Spins Around', 'Until I Found You', 'Mastermind', "Victoria's Secret", 'Labyrinth', "Star Walkin' (League Of Legends Worlds Anthem)", 'Son Of A Sinner', "Don't Come Lookin'", 'Sweet Nothing', 'Romantic Homicide', 'California Breeze', 'Billie Eilish.', 'Wait In The Truck', "Would've, Could've, Should've", 'Heyy', 'Wishful Drinking', 'Free Mind', 'Music For A Sushi Restaurant', 'No Se Va', 'Whiskey On You', 'My Mind & Me', 'Forever', 'Bigger Than The Whole Sky', 'All Mine', 'The Great War', 'Calm Down', 'Miss You', 'Paris', 'Last Last', 'Freestyle', 'Pick Me Up', 'In My Head', 'Country On', 'La Bachata', '2 Be Loved (Am I Ready)', 'High Infidelity', 'Gotta Move On', 'Staying Alive']
+# get_track_ids(tracks)
+print(get_track_ids(tracks))
+
+def add_track(track_ids):
+    # sp.user_playlist_add_tracks(user=user_id, playlist_id=date_playlist_id, )
+    pass
