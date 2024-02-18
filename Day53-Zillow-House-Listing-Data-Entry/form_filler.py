4582a1789cfa452defb1e080c0105ab50231fedc
ShazadO <shazadoutar8@gmail.com>
2024-02-18 03:16:10 -0500

    Bug Fix
    Added a sleep statement to fix the random crashing after around 30 forms.

--- Day53-Zillow-House-Listing-Data-Entry/form_filler.py
+++ Day53-Zillow-House-Listing-Data-Entry/form_filler.py
@@ -1,3 +1,4 @@
+from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from listing_model import Listing
@@ -25,6 +26,8 @@ def fill_forms(house_listings: list[Listing]):
         driver = webdriver.Chrome(options=chrome_options)
         driver.get(url=Form_link)
 
+        # give the page some time to load
+        sleep(2)
         # fill in the input fields
         address_field = driver.find_element(By.CLASS_NAME, value="Xb9hP").find_element(By.XPATH,
                                                                                        value="/html/body/div/div[2]/form/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input")
@@ -37,6 +40,7 @@ def fill_forms(house_listings: list[Listing]):
         link_field = driver.find_element(By.XPATH,
                                          value="/html/body/div/div[2]/form/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input")
         link_field.send_keys(house.link)
+        # link_field.send_keys(Keys.ENTER)
 
         # Press the submit button
         submit_button = driver.find_element(By.XPATH,
