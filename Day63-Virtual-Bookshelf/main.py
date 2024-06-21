32e5d56639b3596fb99e9b9e900d695242f1c5ed
ShazadO <shazadoutar8@gmail.com>
2024-06-21 03:04:02 -0400

    Day 63 Before Adding SQLite

--- Day63-Virtual-Bookshelf/main.py
+++ Day63-Virtual-Bookshelf/main.py
@@ -1,18 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for
 
-
 app = Flask(__name__)
 
 all_books = []
 
-
-# all_books = [
-#      {
-#         "title": "Harry Potter",
-#         "author": "J. K. Rowling",
-#         "rating": 9,
-#     }
-# ]
 
 @app.route('/')
 def home():
@@ -23,18 +14,18 @@ def home():
 def add():
     if request.method == "POST":
         form_data = request.form
-        # title = form_data['title']
-        # all_books.append(title)
+        # print(form_data)
+        # print(form_data["Name"])
         book = {
-            'title': form_data['title'],
-            'author': form_data['author'],
-            'rating': form_data['rating']
+            "title": form_data["title"],
+            "author": form_data["author"],
+            "rating": form_data["rating"]
         }
         all_books.append(book)
-        print(all_books)
         return redirect(url_for('home'))
     return render_template("add.html")
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
