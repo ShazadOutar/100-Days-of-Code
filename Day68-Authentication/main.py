e28286b3da7fa002add9a15955e79a55ac816ecd
ShazadO <shazadoutar8@gmail.com>
2025-01-09 00:17:57 -0500

    Day 68 Completed
    Signed in users no longer see the register and log in buttons. Users not signed in, will now not see the log out button.

--- Day68-Authentication/main.py
+++ Day68-Authentication/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory
+from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory, session
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
@@ -50,42 +50,52 @@ def home():
     return render_template("index.html")
 
 
-@app.route('/register', methods=["GET","POST"])
+@app.route('/register', methods=["GET", "POST"])
 def register():
     if request.method == "POST":
-        # print(request.form.get('name'))
-        form_data = request.form
-        name = form_data.get("name")
-        email = form_data.get("email")
-        password = generate_password_hash(
-            password=form_data.get("password"),
-            method='pbkdf2:sha256',
-            salt_length=8)
-        new_user = User(
-            email=email,
-            name=name,
-            password=password
-        )
-        db.session.add(new_user)
-        db.session.commit()
-        login_user(user=new_user)
-        flash("Success")
-        return render_template('secrets.html', name=name)
+        # check if the email entered is already saved in the db
+        # query = db.session.query(db.exists().where(User.email == request.form.get("email"))).scalar()
+        result = db.session.execute(db.select(User).where(User.email == request.form.get("email")))
+        query = result.scalar()
+        # query returns none if there was no row found with the same email as the input email
+        print(query)
+        if query:
+            flash("Email in use, please log in instead")
+            return redirect("login")
+        else:
+            hash_and_salted_password = generate_password_hash(
+                request.form.get('password'),
+                method='pbkdf2:sha256',
+                salt_length=8
+            )
+            new_user = User(
+                email=request.form.get('email'),
+                name=request.form.get('name'),
+                password=hash_and_salted_password,
+            )
+
+            db.session.add(new_user)
+            db.session.commit()
+
+            # Log in and authenticate user after adding details to database.
+            login_user(new_user)
+
+            # Can redirect() and get name from the current_user
+            return redirect(url_for("secrets"))
+
     return render_template("register.html")
 
 
 @app.route('/login', methods=["GET", "POST"])
-#TODO:
-# Take the user to the login page, and check their username and password
 def login():
     # check if the email is found in the database
     # print(request.form.get("email"))
     if request.method == "POST":
         # print(request.form.get("email"))
         # All emails saved are unique so we just need to find a match
-        user = db.session.query(User).filter(User.email == request.form.get("email")).first()
-        # user = db.session.query(User).get()
-        # print(user)
+        result = db.session.execute(db.select(User).where(User.email == request.form.get("email")))
+        user = result.scalar()
+
         if user:
             # the email did find a match in the db, then check the password
             # print(request.form.get("password"))
@@ -100,25 +110,26 @@ def login():
                 else:
                     print("Didn't work")
                 # - go to the secrets page
-                return render_template("secrets.html", name=user.name)
+                return redirect(url_for("secrets"))
+                # return render_template("secrets.html", name=user.name)
             else:
-                print("incorrect password")
+                print("Incorrect password")
+                flash("Incorrect password")
+                return render_template("login.html")
         else:
             # if the email did not find a match, then tell them they need to sign up
             print("User email not found")
-            return render_template("index.html")
+            flash("That email is not registered, please try again")
+            return render_template("login.html")
 
-    # check if the password entered matches the password saved. remember to hash it before comparing to the saved password
-    # Use this for after they log in successfully
-
-
-    # return redirect(url_for('home'))
     return render_template("login.html")
 
 
 @app.route('/secrets')
 @login_required
 def secrets():
+    # You can get user info from flask login tools
+    print(current_user.name)
     return render_template("secrets.html")
 
 
